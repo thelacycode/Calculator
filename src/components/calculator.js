e06d6ee42369e63404151686f3d9b673ddc2e842
Ty <thelacycode@gmail.com>
2018-01-25 22:10:09 -0500

    calculator css

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -4,35 +4,39 @@ import '../styles/css/calculator.css';
 class Calculator extends React.Component {
   render() {
     return(
-      <div className="calculator-frame">
-        <div className="calculator-top">
-          <div className="calc-buttons calculator-top-buttons">AC</div>
-          <div className="calc-buttons calculator-top-buttons">&plusmn;</div>
-          <div className="calc-buttons calculator-top-buttons">%</div>
+      <div className="calculator">
+        <div className="calc-display">0</div>
+          <div className="calculator-frame">
+            <div className="calc-left">
+              <div className="calculator-top">
+                <div className="calc-keys calculator-top-keys">AC</div>
+                <div className="calc-keys calculator-top-keys">&plusmn;</div>
+                <div className="calc-keys calculator-top-keys">%</div>
+              </div>
+
+              <div className="digit-wrapper">
+                <div className="calc-keys digit-9" onClick = {() =>  this.digitValue(9)}>9</div>
+                <div className="calc-keys digit-8" onClick = {() =>  this.digitValue(8)}>8</div>
+                <div className="calc-keys digit-7" onClick = {() =>  this.digitValue(7)}>7</div>
+                <div className="calc-keys digit-6" onClick = {() =>  this.digitValue(6)}>6</div>
+                <div className="calc-keys digit-5" onClick = {() =>  this.digitValue(5)}>5</div>
+                <div className="calc-keys digit-4" onClick = {() =>  this.digitValue(4)}>4</div>
+                <div className="calc-keys digit-3" onClick = {() =>  this.digitValue(3)}>3</div>
+                <div className="calc-keys digit-2" onClick = {() =>  this.digitValue(2)}>2</div>
+                <div className="calc-keys digit-1" onClick = {() =>  this.digitValue(1)}>1</div>
+                <div className="calc-keys digit-0" onClick = {() =>  this.digitValue(0)}>0</div>
+                <div className="calc-keys dot-key">.</div>
+              </div>
+            </div>
+            <div className="calculator-expressions">
+              <div className="calc-keys exp-keys divde-key">&divide;</div>
+              <div className="calc-keys exp-keys times-key">&times;</div>
+              <div className="calc-keys exp-keys minus-key">-</div>
+              <div className="calc-keys exp-keys plus-key">+</div>
+              <div className="calc-keys exp-keys equal-key">=</div>
+            </div>
+          </div>
         </div>
-
-        <div className="digit-wrapper">
-
-          <div className="calc-buttons calculator-digits" onClick = {() =>  this.digitValue(9)}>9</div>
-          <div className="calc-buttons calculator-digits" onClick = {() =>  this.digitValue(8)}>8</div>
-          <div className="calc-buttons calculator-digits" onClick = {() =>  this.digitValue(7)}>7</div>
-          <div className="calc-buttons calculator-digits" onClick = {() =>  this.digitValue(6)}>6</div>
-          <div className="calc-buttons calculator-digits" onClick = {() =>  this.digitValue(5)}>5</div>
-          <div className="calc-buttons calculator-digits" onClick = {() =>  this.digitValue(4)}>4</div>
-          <div className="calc-buttons calculator-digits" onClick = {() =>  this.digitValue(3)}>3</div>
-          <div className="calc-buttons calculator-digits" onClick = {() =>  this.digitValue(2)}>2</div>
-          <div className="calc-buttons calculator-digits" onClick = {() =>  this.digitValue(1)}>1</div>
-          <div className="calc-buttons calculator-digits zero" onClick = {() =>  this.digitValue(0)}>0</div>
-          <div className="calc-buttons calculator-dot">.</div>
-        </div>
-
-        <div className="calc-buttons calculator-expressions">&divide;</div>
-        <div className="calc-buttons calculator-expressions">&times;</div>
-        <div className="calc-buttons calculator-expressions">-</div>
-        <div className="calc-buttons calculator-expressions">+</div>
-        <div className="calc-buttons calculator-expressions">=</div>
-      </div>
-
 
     )
 }
